2a3ed4932a7146dbd6ca74ab65bf09a8f6ee0ba3
bestcoder0612 <bestcoder0612@gmail.com>
2021-01-16 05:27:29 -0800

    feat: added charts.ts

---
+++ src/static-data/charts.ts
@@ -0,0 +1,29 @@
+export const areaChartData = [
+  {
+    name: 'Germany',
+    series: [
+      {
+        name: '2010',
+        value: 7300000
+      },
+      {
+        name: '2011',
+        value: 8940000
+      }
+    ]
+  },
+
+  {
+    name: 'USA',
+    series: [
+      {
+        name: '2010',
+        value: 7870000
+      },
+      {
+        name: '2011',
+        value: 8270000
+      }
+    ]
+  }
+];
