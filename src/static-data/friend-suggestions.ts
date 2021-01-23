ec7cfb5656c679d9eb2ae378a2a9efc1e4091145
bestcoder0612 <bestcoder0612@gmail.com>
2021-01-23 02:22:13 -0800

    feat: added friend-suggestions.ts

---
+++ src/static-data/friend-suggestions.ts
@@ -0,0 +1,52 @@
+import { FriendSuggestion } from '../app/pages/pages/profile/profile.component';
+
+export const friendSuggestions: FriendSuggestion[] = [
+  {
+    name: 'Fannie Wilson',
+    imageSrc: 'assets/img/avatars/1.jpg',
+    friends: 6,
+    added: false
+  },
+  {
+    name: 'Priscilla Ayala',
+    imageSrc: 'assets/img/avatars/2.jpg',
+    friends: 3,
+    added: false
+  },
+  {
+    name: 'Margaret Farley',
+    imageSrc: 'assets/img/avatars/3.jpg',
+    friends: 3,
+    added: true
+  },
+  {
+    name: 'Alston Tucker',
+    imageSrc: 'assets/img/avatars/4.jpg',
+    friends: 1,
+    added: true
+  },
+  {
+    name: 'Frye Humphrey',
+    imageSrc: 'assets/img/avatars/5.jpg',
+    friends: 1,
+    added: true
+  },
+  {
+    name: 'Jannie Cochran',
+    imageSrc: 'assets/img/avatars/6.jpg',
+    friends: 5,
+    added: false
+  },
+  {
+    name: 'Perkins Doyle',
+    imageSrc: 'assets/img/avatars/7.jpg',
+    friends: 7,
+    added: false
+  },
+  {
+    name: 'Marci Boyd',
+    imageSrc: 'assets/img/avatars/8.jpg',
+    friends: 2,
+    added: false
+  }
+];
